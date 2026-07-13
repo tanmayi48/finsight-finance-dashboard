9f70936b7b0d8dee125491ba09db3c242a066c75
Tanmayi P <tanmayipannegar@gmail.com>
2026-07-13 22:44:14 +0530

    Fix model import case for Render deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user");
 const generateToken = require("../utils/generateToken");
 
 // Register a new user
