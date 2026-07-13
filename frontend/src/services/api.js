4db689964be9eef59c6542b3cc930c56719f5b42
Tanmayi P <tanmayipannegar@gmail.com>
2026-07-13 23:31:44 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://finsight-backend-or1b.onrender.com/api"
 });
 
 api.interceptors.request.use(
